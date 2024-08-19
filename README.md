8b49989cb82619c31e3b515e2545fbb81e8a1f24
Costi Ciudatu <ciudatu@adobe.com>
2024-08-20 00:06:00 +0300

    Generic FlightTableFactory with a default FlightSqlDriver

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# datafusion-table-provider-flightsql
-FlightSQL TableProvider for DataFusion
+# DataFusion TableProviderFactory for Arrow Flight
+A generic `FlightTableFactory` that can integrate any Arrow Flight RPC service
+as a `TableProviderFactory`. Relies on a `FlightDriver` trait implementation to
+handle the `GetFlightInfo` call and all its prerequisites.
+
+## Flight SQL
+This crate includes a `FlightSqlDriver` that has been tested with
+[Ballista](https://github.com/apache/datafusion-ballista),
+[Dremio](https://github.com/dremio/dremio-oss) and
+[ROAPI](https://github.com/roapi/roapi).
---
+++ examples/flight-sql.rs
@@ -0,0 +1,56 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use datafusion::prelude::SessionContext;
+use datafusion_table_provider_flight::sql::FlightSqlDriver;
+use datafusion_table_provider_flight::FlightTableFactory;
+use std::sync::Arc;
+
+/// Prerequisites:
+/// ```
+/// $ brew install roapi
+/// $ roapi -t taxi=https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet
+/// ```
+#[tokio::main]
+async fn main() -> datafusion::common::Result<()> {
+    let ctx = SessionContext::new();
+    ctx.state_ref().write().table_factories_mut().insert(
+        "FLIGHT_SQL".into(),
+        Arc::new(FlightTableFactory::new(
+            Arc::new(FlightSqlDriver::default()),
+        )),
+    );
+    let _ = ctx
+        .sql(r#"
+            CREATE EXTERNAL TABLE trip_data STORED AS FLIGHT_SQL
+            LOCATION 'http://localhost:32010'
+            OPTIONS (
+                'flight.sql.query' 'SELECT * FROM taxi'
+            )
+        "#)
+        .await?;
+
+    let df = ctx
+        .sql(r#"
+            SELECT "VendorID", COUNT(*), SUM(passenger_count), SUM(total_amount)
+            FROM trip_data
+            GROUP BY "VendorID"
+            ORDER BY COUNT(*) DESC
+        "#)
+        .await?;
+    df.show().await
+}
